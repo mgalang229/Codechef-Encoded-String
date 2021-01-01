f7efcfeb1c1baa0ba47ab7ff59c834736ada7f46
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-01-01 21:21:51 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -1,48 +1,50 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int Power(int a, int b) {
-	int res = 1;
-	while (b > 0) {
-		if (b % 2 == 1) {
-			res *= a;
-		}
-		a *= a;
-		b /= 2;
+#define ll long long
+#define ar array
+
+int power(int a, int b) {
+	int res=1;
+	while(b) {
+		if(b&1)
+			res*=a;
+		a*=a;
+		b/=2;
 	}
 	return res;
 }
 
-char Extract(string s) {
-	int tmp = stoi(s);
-	int rem = 0;
-	int d = 0;
-	for (int i = 0; tmp != 0; i++) {
-		rem = tmp % 10;
-		tmp /= 10;
-		d += rem * Power(2, i);
+char cnv(string s) {
+	int tmp=stoi(s);
+	int rem=0, d=0;
+	for(int i=0; tmp!=0; ++i) {
+		rem=tmp%10;
+		tmp/=10;
+		d+=rem*power(2, i);
 	}
-	return char(d + 97);
+	return char(d+97);
+}
+
+void solve() {
+	int n;
+	cin >> n;
+	string s;
+	cin >> s;
+	string tmp="";
+	//take every four characters of the string and convert it from binary to decimal
+	//after conversion, add its corresponding character equivalent from the alphabet
+	for(int i=0; i<n; i+=4)
+		tmp+=cnv(s.substr(i, 4));
+	cout << tmp << "\n";
 }
 
 int main() {
-	ios::sync_with_stdio(false);
+	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int tt;
-	cin >> tt;
-	while (tt--) {
-		int n;
-		cin >> n;
-		string s;
-		cin >> s;
-		string tmp = "";
-		// take every four characters of the string and convert it from binary to decimal
-		// after conversion, add its corresponding character equivalent from the alphabet
-		for (int i = 0; i < n; i += 4) {
-			tmp += Extract(s.substr(i, 4));
-		}
-		cout << tmp << '\n';
-	}
-	return 0;	
+	
+	int t;
+	cin >> t;
+	while(t--)
+		solve();
 }
